f76a9c1731b464c286bd260f17e311011461df35
alklid <alklid@gmail.com>
2022-11-04 08:11:05 +0900

    init project

---
+++ src/main/java/me/sandbox/alklid/SandboxDbConnectApplication.java
@@ -0,0 +1,13 @@
+package me.sandbox.alklid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxDbConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandboxDbConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/sandbox/alklid/SandboxDbConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sandbox.alklid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxDbConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
